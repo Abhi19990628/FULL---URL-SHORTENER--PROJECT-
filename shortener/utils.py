848f3fdae5f84255f26ee0fc172f716b3281e611
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-05-03 17:37:18 +0530

    Update utils.py

--- shortener/utils.py
+++ shortener/utils.py
@@ -2,7 +2,7 @@ import string
 import random
 
 def generate_short_url():
-    # Generate a random string of alphanumeric characters for the short URL
+
     characters = string.ascii_letters + string.digits
     short_url = ''.join(random.choice(characters) for _ in range(6))  # Adjust the length as needed # Adjust the length as needed
     return short_url
